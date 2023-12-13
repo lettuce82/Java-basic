78aa857a9e1e892d5dfd225acdb6c6e4cef6ee21
lettuce <chaesanghui142@gmail.com>
2023-12-13 11:09:58 +0900

    12/11 - 생성자 & 문풀

---
+++ src/construct/MemberInit.java
@@ -0,0 +1,15 @@
+package construct;
+
+public class MemberInit {
+    String name;
+    int age;
+    int grade;
+    
+    //추가
+    void initMember(MemberInit member, String name, int age, int grade) {
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+    }
+}
+
