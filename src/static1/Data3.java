2ff0703fd9d5891c7e46734226238611cb947a23
lettuce <chaesanghui142@gmail.com>
2023-12-14 16:00:41 +0900

    12/11 - static

---
+++ src/static1/Data3.java
@@ -0,0 +1,12 @@
+package static1;
+
+public class Data3 {
+    public String name;
+    public static int count; //static변수 = 정적 변수 = 클래스 변수
+    //static 변수
+
+    public Data3(String name) {
+        this.name = name;
+        count++;
+    }
+}
---
+++ src/static2/DecoMain1.java
@@ -0,0 +1,12 @@
+package static2;
+
+public class DecoMain1 {
+    public static void main(String[] args) {
+        String s = "hello java";
+        DecoUtill utills = new DecoUtill();
+        String deco = utills.deco(s);
+
+        System.out.println("before: " + s);
+        System.out.println("after: " + deco);
+    }
+}
